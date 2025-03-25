35a0dc9e4e82a9ad020d624727efe08141e6e6c3
Benjamin <benabolade@gmail.com>
2025-03-25 16:00:04 +0100

    (chore) update build error

--- app/reviews/[book]/page.tsx
+++ app/reviews/[book]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ReviewsPage = ({ params }: { params: Promise<{ book: string }> }) => {
+const ReviewsPage = async ({ params }: { params: Promise<{ book: string }> }) => {
   const { book } = await params;
   return <div>{book}</div>;
 };
