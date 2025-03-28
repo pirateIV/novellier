f64703995dfc240435d49306155ee51ee9b5de92
Benjamin <benabolade@gmail.com>
2025-03-28 02:57:28 +0100

    (ui): update loading state

--- app/genres/[genre]/page.tsx
+++ app/genres/[genre]/page.tsx
@@ -8,6 +8,31 @@ import { genres } from "@/lib/books";
 import GenreHeader from "@/components/genres/GenreHeader";
 import GenreBookList from "@/components/genres/GenreBookList";
 
+const SkeletonLoader = () => (
+  <>
+    {Array(6)
+      .fill(null)
+      .map((_, i) => (
+        <div
+          key={i}
+          className="flex gap-4 py-2 p-1 md:p-3 animate-pulse divide-y divide-slate-950/[.07] dark:divide-white/10"
+        >
+          <div className="h-40 w-[100px] shrink-0 rounded-md bg-gradient-to-r skeleton"></div>
+          <div className="min-h-full flex flex-col flex-1 gap-6">
+            <div className="skeleton w-2/4 h-6 bg-gradient-to-r rounded-md"></div>
+            <div className="flex flex-col gap-2">
+              <div className="skeleton w-full h-3 bg-gradient-to-r rounded-sm"></div>
+              <div className="skeleton w-full h-3 bg-gradient-to-r rounded-sm"></div>
+              <div className="skeleton w-full h-3 bg-gradient-to-r rounded-sm"></div>
+              <div className="skeleton w-8/12 h-3 bg-gradient-to-r rounded-sm"></div>
+              <div className="skeleton w-1/6 h-3 self-end mt-4 bg-gradient-to-r rounded-sm"></div>
+            </div>
+          </div>
+        </div>
+      ))}
+  </>
+);
+
 const GenrePage = () => {
   const { genre } = useParams() as { genre: string };
   const genreData = genres.find((g) => g.slug === genre);
@@ -24,13 +49,13 @@ const GenrePage = () => {
   });
 
   return (
-    <div className="px-2 sm:px-4 md:px-7 py-5 max-w-full">
+    <div className="max-w-full px-2 py-5 md:px-7 sm:px-4">
       <GenreHeader name={name} description={description} />
-      <Suspense
-        fallback={<div className="p-5 text-center">Loading books...</div>}
-      >
+      <Suspense fallback={null}>
         {isLoading ? (
-          <div className="p-5 text-center">Loading books...</div>
+          <div className="mt-5">
+            <SkeletonLoader />
+          </div>
         ) : (
           <GenreBookList subjectData={subjectData} />
         )}
